08cd19da9d10a77665c9ff7ac193498ae8c86280
Mitsunori Komatsu <komamitsu@gmail.com>
2013-03-06 00:32:57 +0900

    call remote procedure in background

--- src/com/komamitsu/android/rpctest/MainActivity.java
+++ src/com/komamitsu/android/rpctest/MainActivity.java
@@ -4,51 +4,65 @@ import java.net.UnknownHostException;
 
 import org.msgpack.rpc.Client;
 import org.msgpack.rpc.loop.EventLoop;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	public static interface RPCInterface {
-		int add(int a, int b);
-	}
+  public static interface RPCInterface {
+    int add(int a, int b);
+  }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onResume()
-	 */
-	@Override
-	protected void onResume() {
-		super.onResume();
-		
-		Logger logger = LoggerFactory.getLogger(this.getClass());
-		logger.info("hogehoge");
-		
-		EventLoop loop = EventLoop	.defaultEventLoop();
-		try {
-			Client client = new Client("192.168.xxx.xxx", 9090, loop);
-			RPCInterface rpcInterface = client.proxy(RPCInterface.class);
-			int result = rpcInterface.add(123, 456);
-			Toast.makeText(this, "result => " + result, Toast.LENGTH_LONG).show();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-	}
+  /* (non-Javadoc)
+   * @see android.app.Activity#onResume()
+   */
+  @Override
+  protected void onResume() {
+    super.onResume();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    /*
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    logger.info("hogehoge");
+    */
+    new AsyncTask<Void, Void, Integer>() {
+      Exception e;
+
+      @Override
+      protected Integer doInBackground(Void... params) {
+        EventLoop loop = EventLoop.defaultEventLoop();
+        try {
+          Client client = new Client("192.168.0.6", 9090, loop);
+          RPCInterface rpcInterface = client.proxy(RPCInterface.class);
+          return rpcInterface.add(123, 456);
+        } catch (UnknownHostException e) {
+          this.e = e;
+          return null;
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Integer result) {
+        String msg = (e == null) ?
+            ("result => " + result) : ("error => " + e.getMessage());
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+      }
+    }.execute();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
 
 }
